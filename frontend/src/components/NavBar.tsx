27aa41d6369b1512500e3eb7f305317ab2f5736d
MaitJ <MaitJ@tlu.ee>
2021-09-30 19:13:13 +0300

    Navbarile kategooriate nupud

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -7,8 +7,10 @@ const NavBar: React.FC = () => {
         <nav>
             <Link to="/">Home</Link>
             <Link to="/items">Items</Link>
-            <Link to="/greeting">Greeting</Link>
             <Link to="/additem">Eseme lisamine</Link>
+            <Link to="/greeting">Greeting</Link>
+            <Link to="/categories">Kategooriad</Link>
+            <Link to="/addcategory">Kategooriate lisamine</Link>
             <img src="shopping-cart.svg" onClick={() => history.push('/cart')}></img>
         </nav>
     );
